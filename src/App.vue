01ac5d9e1d6a2a3103d234840bf30e33513fe703
supershy1230 <supershy1230@shy.com>
2021-03-02 20:30:30 +0800

    全自动部署 第三次

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,6 @@
       <p>java0821 全体同学 牛啊牛啊</p>
       <p>java0821 supershy 孙红元</p>
       <p>java0821 fuck shit pussy</p>
-      <p>java0821 你他妈触发啊，草</p>
    <img src="http://img12.360buyimg.com/n1/s450x450_jfs/t1/142295/6/19866/63874/5fe4408eE401889c8/8f5c2b37676a2264.jpg"/>
   </div>
 </template>
